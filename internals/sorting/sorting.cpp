9e34e92a755d7bfb4826fd8774bd27b5f89aa3fe
ArshhGill <arshh.gill@outlook.com>
2024-02-08 11:53:39 +0530

    implement bubbleSort

---
+++ internals/sorting/sorting.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <utility>
+#include "vector"
+using namespace std;
+
+// PADHAI ARC
+
+
+vector<int> bubbleSort(vector<int> l){
+    while (true){
+        int c{0};
+        for (int i = 0; i < l.size()-1; i ++){
+            if (l[i] > l[i+1]){
+                swap(l[i], l[i+1]);
+                c++;
+            }
+        }
+        if (c == 0){
+            break;
+        }
+    }
+    return l;
+}
+
+void bubbleSortRunner(vector<int> l){
+
+    vector<int> bubbleSortReturn = bubbleSort(l);
+    for (int i = 0; i < bubbleSortReturn.size(); i++){
+        cout << bubbleSortReturn[i] << " ";
+    }
+}
+
+int main(){
+    vector<int> l = {1,5,3,7,3,6,8,43,0};
+
+    for (int i = 0; i < l.size(); i++){
+        cout << l[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSortRunner(l);
+
+    return 0;
+}
+
